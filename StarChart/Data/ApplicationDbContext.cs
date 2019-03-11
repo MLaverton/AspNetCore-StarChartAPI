ffd017610adb60400b091577a2c4c80e283c2144
MLaverton <33700030+MLaverton@users.noreply.github.com>
2019-03-11 08:22:13 +0000

    Module 3 complete

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -2,10 +2,14 @@
 
 namespace StarChart.Data
 {
+    using StarChart.Models;
+
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
+
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
     }
 }
